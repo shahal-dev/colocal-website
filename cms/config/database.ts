4da5df6247f7652d951713206818a4b16f074719
Adib Ashraf <aa.adibashraf@gmail.com>
2025-09-25 22:25:35 +0600

    ssl changed

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -33,12 +33,12 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'colocal_db'),
+        host: env('DATABASE_HOST' ),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        ssl: env.bool('DATABASE_SSL', true) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
